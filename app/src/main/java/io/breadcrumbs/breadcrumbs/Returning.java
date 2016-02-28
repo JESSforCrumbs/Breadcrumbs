702a18ad96d1406058337dbab1c825d0cab7bf26
Jocelyn Lee <jocelynjyl@users.noreply.github.com>
2016-02-28 00:59:52 -0800

    Complete UI

--- app/src/main/java/io/breadcrumbs/breadcrumbs/Returning.java
+++ app/src/main/java/io/breadcrumbs/breadcrumbs/Returning.java
@@ -1,11 +1,20 @@
 package io.breadcrumbs.breadcrumbs;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Returning extends AppCompatActivity {
 
@@ -16,15 +25,46 @@ public class Returning extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        TextView t = (TextView) findViewById(R.id.distance);
+        t.setText("300 m");
+
+        TextView u = (TextView) findViewById(R.id.time);
+        u.setText("10 min");
+
+        ImageView i = (ImageView) findViewById(R.id.arrow);
+        i.setRotation(90);
+        i.setColorFilter(Color.BLUE);
+        //Drawable.setColorFilter(Color.BLUE , PorterDuff.Mode.MULTIPLY);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Drop Crumbs button */
+    public void startWalking(View view) {
+        Intent intent = new Intent(this, Walking.class);
+        startActivity(intent);
     }
 
 }
