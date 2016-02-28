5c38ef1c200fcbf73dae63dc27ed909d4b355452
Helisquinde <stebokan@gmail.com>
2016-02-28 09:31:15 -0800

    Added listeners to Returning and added method to update myBearing value

--- app/src/main/java/io/breadcrumbs/breadcrumbs/Returning.java
+++ app/src/main/java/io/breadcrumbs/breadcrumbs/Returning.java
@@ -1,15 +1,25 @@
 package io.breadcrumbs.breadcrumbs;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.media.Image;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -17,10 +27,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class Returning extends AppCompatActivity {
+public class Returning extends AppCompatActivity implements LocationListener, SensorEventListener {
     private LocationManager locationManager;
     private String provider = "gps";
     private boolean enabled;
@@ -28,12 +39,16 @@ public class Returning extends AppCompatActivity {
     private ArrayList<Location> locations;
     private float distance;
 
+    private float myBearing;
+    private float compassBearing;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_returning);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
         Bundle extras = getIntent().getExtras();
         //TODO: what if locations was null (i.e. nothing got saved)
@@ -51,7 +66,127 @@ public class Returning extends AppCompatActivity {
         i.setColorFilter(Color.BLUE);
         //Drawable.setColorFilter(Color.BLUE , PorterDuff.Mode.MULTIPLY);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        // Initialize the location fields
+        if (location != null) {
+            onLocationChanged(location);
+        }
+
+        enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!enabled) {
+            // Create a dialog here that requests the user to enable GPS, and use an intent
+            // with the android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS action
+            // to take the user to the Settings screen to enable GPS when they click "OK"
+            new AlertDialog.Builder(this)
+                    .setTitle("Enable GPS")
+                    .setMessage("This Application requires the use of Mobile GPS. " +
+                            "Do you wish to enable this feature")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // show system settings
+                            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // do nothing
+                        }
+                    })
+                    .show();
+        }
+    }
+
+    /* Request updates at startup */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    /* Remove the locationlistener updates when Activity is paused */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+
+        if (locations != null){
+            Location lastLocation = locations.get(locations.size()-1);
+            if(lastLocation.distanceTo(location) >= 10) {
+                //TODO: need to change updateDist to subtract distance when necessary
+                //TODO: need to figure out when to remove current lastLocation
+                updateDist();
+                myBearing = location.bearingTo(lastLocation);
+            }
+        }
+        else{
+            locations = new ArrayList<>();
+            locations.add(location);
+        }
+
+    }
+
+    public void updateDist() {
+        Location currLoc = locations.get(locations.size()-1);
+        Location prevLoc = locations.get(locations.size()-2);
+        distance += prevLoc.distanceTo(currLoc);
+        TextView t = (TextView) findViewById(R.id.distance);
+        t.setText(distance + " m");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -81,4 +216,13 @@ public class Returning extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
